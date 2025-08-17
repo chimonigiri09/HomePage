7317143ae1b5a30cd9e3e085cf0a22789177f8a8
DocteuMan <name.jellybeans511>
2025-08-17 18:57:06 +0900

    second commit!!!
    Co-authored-by: chimonigiri09 <chimonigiri09@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  server: {
+    host: true,
+    port: 4321, // 任意
+  },
+  vite: {
+    server: {
+      // Live Share / プロキシ越しでHMRが切れる対策
+      hmr: {
+        clientPort: 443,
+        protocol: 'wss', // 必要に応じて。まずは付けてOK
+      },
+    },
+  },
 });
